8e1e40fb7e85a43ee6377c0313a252f8797dc380
Carlos <charly.2701997@gmail.com>
2021-01-12 18:53:48 -0500

    base

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cocoa';
 } else {
-    urlDB = "mongodb+srv://cocoa-user:Programar.1234@cluster0.azijh.mongodb.net/cocoa"
+    urlDB = "mongodb+srv://cocoa:Programar.1234@cluster0.azijh.mongodb.net/cocoa"
 }
 
 process.env.URLDB = urlDB;
